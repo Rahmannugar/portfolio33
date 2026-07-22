9cec83d0dea6bccd3e5e6e2ae3d3f4e858092ae6
Rahmannugar <cladeadenugar@gmail.com>
2026-07-22 11:57:23 +0100

    ui: update company name styles

--- components/custom-ui/company-link.tsx
+++ components/custom-ui/company-link.tsx
@@ -13,7 +13,7 @@ const CompanyLink = ({ company, companyUrl }: CompanyLinkProps) => {
       href={companyUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="underline decoration-purple-300 underline-offset-4 transition-colors hover:text-white"
+      className="underline decoration-purple-300 text-purple-300 underline-offset-4 transition-colors hover:text-white"
     >
       {company}
     </Link>
